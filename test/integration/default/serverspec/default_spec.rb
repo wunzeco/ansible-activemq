85d84e670cf412b1505133a734a99ea0f3f4c797
Ogonna Iwunze <ogonna.iwunze@gmail.com>
2017-06-08 10:41:37 +0100

    [OI] update integration tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -4,6 +4,21 @@ describe package('activemq') do
   it { should be_installed }
 end
 
-#describe service('influxdb') do
-#  it { should be_running }
-#end
+%w(
+  /etc/activemq/instances-available/main/activemq.xml
+  /etc/activemq/instances-available/main/log4j.properties
+).each do |f|
+  describe file(f) do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_mode 644 }
+  end
+end
+
+describe file("/etc/activemq/instances-enabled/main") do
+  it { should be_symlink }
+end
+
+describe service('activemq') do
+  it { should be_running }
+end
