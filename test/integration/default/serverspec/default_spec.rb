b93c5d947b95d8f870213f6f8961c431aa0cb19f
dmotwani <dmotwani@equalexperts.com>
2017-07-04 15:11:05 +0530

    OCP-1056 [DM] Changed installation of activemq from package manager to binary installation

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -5,17 +5,31 @@ describe package('activemq') do
 end
 
 %w(
-  /etc/activemq/instances-available/main/activemq.xml
-  /etc/activemq/instances-available/main/log4j.properties
+  /opt/activemq
+  /opt/activemq/conf
+  /var/lib/activemq/data
+  /opt/activemq/tmp
 ).each do |f|
   describe file(f) do
-    it { should be_file }
-    it { should be_owned_by 'root' }
-    it { should be_mode 644 }
+    it { should be_directory }
+    it { should be_owned_by 'activemq' }
+    it { should be_mode 775 }
   end
 end
 
-describe file("/etc/activemq/instances-enabled/main") do
+describe file("/var/log/activemq") do
+  it { should be_directory }
+  it { should be_owned_by 'activemq' }
+  it { should be_mode 755 }
+end
+
+describe file("/etc/default/activemq") do
+  it { should be_file }
+  it { should be_owned_by 'root' }
+  it { should be_mode 644 }
+end
+
+describe file("/etc/init.d/activemq") do
   it { should be_symlink }
 end
 
